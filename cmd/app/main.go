d49b4f42b23597b1073f4577b649258e49d1105c
nvsick <nvs.holy@gmail.com>
2025-02-12 14:49:16 +0300

    add main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "avito-shop/internal/app"
 
+// TODO: think about adding as flag
+
+const configPath = "./config/config.yaml"
+
+func main() {
+	app.Run(configPath)
 }
