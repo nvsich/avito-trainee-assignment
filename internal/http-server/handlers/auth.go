0f06a3a92eaf4076900e74c1af7c8cea1716e550
nvsick <nvs.holy@gmail.com>
2025-02-12 14:46:39 +0300

    add auth handler

---
+++ internal/http-server/handlers/auth.go
@@ -0,0 +1,77 @@
+package handlers
+
+import (
+	req "avito-shop/internal/http-server/dto/request"
+	resp "avito-shop/internal/http-server/dto/response"
+	"avito-shop/internal/lib/logger/sl"
+	"avito-shop/internal/service"
+	"context"
+	"errors"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	"log/slog"
+	"net/http"
+)
+
+type Auth interface {
+	Authorize(ctx context.Context, login string, password string) (string, error)
+}
+
+func NewAuthHandlerFunc(log *slog.Logger, authService Auth) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "http-server.handlers.auth.New"
+
+		log = log.With(
+			slog.String("operation", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var request req.AuthRequest
+
+		err := render.DecodeJSON(r.Body, &request)
+		if err != nil {
+			log.Error("failed to parse request", sl.Err(err))
+
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, resp.ErrorResponse{Errors: "failed to parse request"})
+			return
+		}
+
+		log.Debug("request body successfully decoded", slog.String("username", request.Username))
+
+		if err = validator.New().Struct(request); err != nil {
+			var validateErr validator.ValidationErrors
+			errors.As(err, &validateErr)
+			log.Error("invalid request", slog.String("validation_error", validateErr.Error()))
+
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, resp.ErrorResponse{Errors: "invalid request body"})
+			return
+		}
+
+		log.Debug("validation passed", slog.String("username", request.Username))
+
+		token, err := authService.Authorize(r.Context(), request.Username, request.Password)
+		if err != nil {
+			if errors.Is(err, service.ErrInvalidCredentials) {
+				log.Info("invalid login attempt", slog.String("username", request.Username))
+
+				render.Status(r, http.StatusUnauthorized)
+				render.JSON(w, r, resp.ErrorResponse{Errors: "invalid credentials"})
+				return
+			}
+
+			log.Error("failed to authorize", sl.Err(err))
+
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, resp.ErrorResponse{Errors: "failed to authorize"})
+			return
+		}
+
+		log.Info("user successfully authenticated", slog.String("username", request.Username))
+
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, resp.AuthResponse{Token: token})
+	}
+}
