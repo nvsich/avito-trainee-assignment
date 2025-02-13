2560fa6985e49edefc8c8586104adba2c31d2dcb
nvsick <nvs.holy@gmail.com>
2025-02-13 16:48:44 +0300

    add PGItemRepo

---
+++ internal/repo/pgdb/item.go
@@ -0,0 +1,49 @@
+package pgdb
+
+import (
+	"avito-shop/internal/model"
+	"avito-shop/internal/repo"
+	"context"
+	"errors"
+	"fmt"
+	trmpgx "github.com/avito-tech/go-transaction-manager/drivers/pgxv5/v2"
+	"github.com/jackc/pgx/v5"
+)
+
+type PGItemRepo struct {
+	*Postgres
+	getter *trmpgx.CtxGetter
+}
+
+func NewPGItemRepo(p *Postgres, c *trmpgx.CtxGetter) *PGItemRepo {
+	return &PGItemRepo{p, c}
+}
+
+func (r *PGItemRepo) FindByName(ctx context.Context, itemName string) (*model.Item, error) {
+	const op = "repo.pgdb.PGItemRepo.FindByName"
+
+	query, args, err := r.Builder.
+		Select("id, name, price").
+		From("items").
+		Where("name = ?", itemName).
+		ToSql()
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	conn := r.getter.DefaultTrOrDB(ctx, r.Pool)
+
+	var item model.Item
+	err = conn.QueryRow(ctx, query, args...).
+		Scan(&item.Id, &item.Name, &item.Price)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, repo.ErrItemNotFound
+		}
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &item, nil
+}
