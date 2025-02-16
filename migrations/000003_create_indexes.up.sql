50dd3d44cf068efdc2929c4c395acf0541c6956f
nvsick <nvs.holy@gmail.com>
2025-02-16 14:57:38 +0300

    fix indexes for transfers

--- migrations/000003_create_indexes.up.sql
+++ migrations/000003_create_indexes.up.sql
@@ -8,5 +8,5 @@ create unique index if not exists items_name_idx on items (name);
 create unique index if not exists employee_inventory_employee_item_idx on employee_inventory (employee_id, item_id);
 
 -- transfers table
-create unique index if not exists transfers_to_employee_from_employee_idx on transfers (to_employee, from_employee);
-create unique index if not exists transfers_from_employee_to_employee_idx on transfers (from_employee, to_employee);
+create index if not exists transfers_to_employee_from_employee_idx on transfers (to_employee, from_employee);
+create index if not exists transfers_from_employee_to_employee_idx on transfers (from_employee, to_employee);
